2d015598915c6aba2865225ce1463f024460f1ed
shayNicol <shaynicolaslove@gmail.com>
2023-02-23 17:38:25 -0500

    added Auth

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const { User, Quote } = require('./db');
+const JWT_SECRET = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-
-
 
 
 
@@ -48,7 +48,23 @@ app.post('/register', async (req, res) => {
   });
 
 
-app.get('/Quote',async (req, res, next) => {
+
+
+   //Authentication middleware
+  const setUser = async (req, res, next) => {
+    const auth = req.header("Authorization");
+    if (!auth) {
+      next();
+    } else {
+      const [, token] = auth.split(" ");
+      const user = jwt.verify(token, JWT_SECRET);
+      req.user = user;
+      next();
+    }
+  };
+
+
+  app.get('/Quote',async (req, res, next) => {
     try {
         res.send(await Quote.findAll());
     }
@@ -58,7 +74,8 @@ app.get('/Quote',async (req, res, next) => {
     }
 });
 
-app.post('/Quote', async (req, res, next) => {
+
+app.post('/Quote', setUser, async (req, res, next) => {
     try {
         res.status(201).send(await Quote.create(req.body));
     }
@@ -68,7 +85,7 @@ app.post('/Quote', async (req, res, next) => {
     }
 });
 
-app.patch('/Quote/:id', async (req, res, next) => {
+app.patch('/Quote/:id', setUser, async (req, res, next) => {
     try {
         const quote = await Quote.findByPk(req.params.id);
         await quote.update(req.body);
@@ -80,7 +97,7 @@ app.patch('/Quote/:id', async (req, res, next) => {
     }
 });
 
-app.delete('/Quote/:id', async (req, res, next) => {
+app.delete('/Quote/:id', setUser, async (req, res, next) => {
     try {
         res.status(204).send(await Quote.destroy({ where: { id: req.params.id } }));
     }
@@ -89,5 +106,9 @@ app.delete('/Quote/:id', async (req, res, next) => {
         next(error);
     }
 });
+
+
+
+
 
 module.exports = app;
