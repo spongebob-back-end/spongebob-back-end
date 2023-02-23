329850a42361603296178e779e71a7855755c46f
unknown <ainouzusa@gmail.com>
2023-02-23 19:00:59 +0100

    creating data for and set all files

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const {User} = require('./User');
+const {sequelize, Sequelize} = require('./db');
+const {Quote} = require('./quotes')
+
+
+
+
+module.exports = {
+    User,
+    Quote,
+    sequelize,
+    Sequelize
+};
