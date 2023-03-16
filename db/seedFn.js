b524e687c148392bc072afe6ce5c5ec7a2f2d671
unknown <ainouzusa@gmail.com>
2023-03-16 18:16:30 +0100

    admin-done-v3

--- db/seedFn.js
+++ db/seedFn.js
@@ -1,14 +1,21 @@
 const {sequelize} = require('./db');
 const {Quote} = require('./quotes.js');
 const {User} = require('./User.js');
-const {quotes} = require('./seedData');
-const {users} = require('./seedData');
-
+const {quotes,users} = require('./seedData');
 
 const seed = async () => {
-  await sequelize.sync({ force: true }); // recreate db
-  await Quote.bulkCreate(quotes);
-  await User.bulkCreate(users);
+  try {
+    await sequelize.sync({ force: true }); // recreate db
+    const createdUsers = await User.bulkCreate(users);
+    const createdQuotes = await Quote.bulkCreate(quotes);
+    for (let i=0; i<createdQuotes.length; ++i) {
+      const quote = createdQuotes[i];
+      const user = createdUsers[i % createdUsers.length].id;
+     //await quote.setUser(user);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = seed;
